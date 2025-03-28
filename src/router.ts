9a3ab10622498102ceaec5c0fa27e08cd95d3ac3
L-Chris <563303226@qq.com>
2025-03-28 17:17:37 +0800

    feat: 注册登录、令牌和搜索路由

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,13 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { registerSendCodeRoute } from "./handlers/sendcode.ts";
+import { registerLoginRoute } from "./handlers/login.ts";
+import { registerTokenRoute } from "./handlers/token.ts";
+import { registerSearchRoute } from "./handlers/search.ts";
 
 
 export const registerRoutes = (app: OpenAPIHono) => {
     registerSendCodeRoute(app)
+    registerLoginRoute(app)
+    registerTokenRoute(app)
+    registerSearchRoute(app)
 }
